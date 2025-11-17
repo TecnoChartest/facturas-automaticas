5b13c0815a0370c2ba7e804dd90dcca55cd56ac2
TecnoChartest <tecnocharles0@gmail.com>
2025-11-17 18:18:11 -0500

    feat(chartest-develop): persistencia en pg y volumen de n8n
    - Agrega carpeta de volumen  para la persistencia del workflow
    - Añade métodos POST en NestJS para subir datos de la factura y la imagen a pg

--- backend/src/facturas/entities/factura.entity.ts
+++ backend/src/facturas/entities/factura.entity.ts
@@ -1,45 +1,46 @@
-/* eslint-disable prettier/prettier */
-import { Usuario } from "src/usuarios/entities/usuario.entity";
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import { Entity, Column, PrimaryColumn } from 'typeorm';
 
-@Entity('factura')
+@Entity({ name: 'factura', schema: 'public' })
 export class Factura {
-
   @PrimaryColumn({ type: 'text' })
   id: string;
 
-  @Column({ name: 'id_usuario' })
-  idUsuario: string;
+  @Column({ type: 'text' })
+  productos: string;
 
-  @ManyToOne(() => Usuario, (usuario) => usuario.facturas)
-  @JoinColumn({ name: 'id_usuario' })
-  usuario: Usuario;
-
-  @Column({ name: 'fecha_generacion', type: 'date' })
+  @Column({ type: 'date', name: 'fecha_generacion' })
   fechaGeneracion: Date;
 
-  @Column({ name: 'fecha_expedicion', type: 'date' })
-  fechaExpedicion: Date;
-
-  @Column({ name: 'fecha_vencimiento', type: 'date' })
+  @Column({ type: 'date', name: 'fecha_vencimiento' })
   fechaVencimiento: Date;
 
-  @Column({ type: 'json' })
-  productos: any;
-
-  @Column({ name: 'metodo_pago', length: 50 })
-  metodoPago: string;
-
-  @Column({ name: 'total_bruto', type: 'int' })
+  @Column({ type: 'int', name: 'total_bruto' })
   totalBruto: number;
 
-  @Column({ name: 'total_iva', type: 'int', nullable: true })
-  totalIva?: number;
+  @Column({ type: 'int', name: 'total_iva', nullable: true })
+  totalIva: number;
 
-  @Column({ name: 'total_pagar', type: 'int' })
+  @Column({ type: 'int', name: 'total_pagar' })
   totalPagar: number;
+
+  @Column({ type: 'varchar', length: 50, name: 'metodo_pago' })
+  metodoPago: string;
 
   @Column({ type: 'text' })
   path: string;
 
+  @Column({ type: 'varchar', length: 50, name: 'nombre_cliente' })
+  nombreCliente: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  cedula: string;
+
+  @Column({ type: 'varchar', length: 250 })
+  direccion: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  telefono: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  ciudad: string;
 }
