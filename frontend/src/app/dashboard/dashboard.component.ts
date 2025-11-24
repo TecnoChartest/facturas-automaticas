7d744df5c288e73b3ac2aefd825111942652fb62
TecnoChartest <tecnocharles0@gmail.com>
2025-11-23 23:07:12 -0500

    feat(estadísticas): implementación completa de panel de estadísticas con Metabase y población de datos en pg
    - Integración de Metabase
    - Población de datos en pg

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { environment } from '../../environments/environment.development';
 
 @Component({
   selector: 'app-dashboard.component',
@@ -7,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.css',
 })
 export default class DashboardComponent {
+  // URL pública de Metabase
+  private metabaseUrl =
+    `${environment.CONSUMIR_METABASE}`;
+  safeIframeUrl!: SafeResourceUrl;
 
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnInit(): void {
+    // Sanitizar la URL para que Angular la considere segura
+    this.safeIframeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.metabaseUrl);
+    console.log('Dashboard URL sanitized:', this.metabaseUrl);
+  }
+
+  ngOnDestroy(): void {
+    // Limpieza si es necesaria
+  }
 }
--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -3,5 +3,6 @@ export const environment = {
   SUBIR_FACTURA_pg: 'http://localhost:4000/facturas',
   SUBIR_CLIENTE_pg: 'http://localhost:4000/clientes',
   SUBIR_PRODUCTO_pg: 'http://localhost:4000/productos',
-  SUBIR_FACTURA_DETALLE_pg: 'http://localhost:4000/factura-detalle'
+  SUBIR_FACTURA_DETALLE_pg: 'http://localhost:4000/factura-detalle',
+  CONSUMIR_METABASE: 'http://localhost:3000/public/dashboard/9e566e50-6381-4fb0-a333-9e6775dd878b',
 };
